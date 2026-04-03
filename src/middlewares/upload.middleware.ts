941805316758c6fcfca0257e88151fb4dda0b712
Tushar Vaghela <tusharvaghela5027@gmail.com>
2026-04-04 00:05:55 +0530

    fix : increase file size in upload

--- src/middlewares/upload.middleware.ts
+++ src/middlewares/upload.middleware.ts
@@ -15,7 +15,7 @@ class UploadMiddleware {
 
         return multer({
             storage,
-            limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+            limits: { fileSize: 10 * 1024 * 1024 }, // 5MB
         })
     }
 
